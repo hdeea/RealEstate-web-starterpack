d8adc51b1cba758a7f1ae0e81b61a0234c0050e8
hdeea <hdeea.ghannom@gmail.com>
2026-01-06 23:53:54 +0300

    Update project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-
+import Header from './components/Header/header';
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
-  );
+<section className="h-wrapper">
+  <div className="h-container">
+    <img src="./logo.png" alt="logo" width={100} />
+ <div className="h-menu">
+  <a href=""></a>
+   <a href=""></a>
+    <a href=""></a>
+     <a href=""></a>
+      <a href=""></a>
+ </div>
+  </div>
+
+</section>
+  )
 }
 
 export default App;
